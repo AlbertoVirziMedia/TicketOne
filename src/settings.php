9b4ce89a6c34b20328bab4b06b0d455a21454191
Alberto Virzì <albertovirzi@MacBook-Pro-di-Alberto.local>
2019-04-15 20:26:28 +0200

    aggiunta configurazione al database con PDO

--- src/settings.php
+++ src/settings.php
@@ -15,5 +15,14 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+
+        'db' => '' [
+
+          'host' => '127.0.0.1',
+          'dbname' => 'TicketOne',
+          'port' => '8889',
+          'user' => 'root',
+          'pass' => 'root'
+        ]
     ],
 ];
